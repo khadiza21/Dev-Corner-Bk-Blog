5eddbbc609529f5df557ddd313b0d21d328b8c48
khadiza21 <bibikhadiza474@gmail.com>
2024-10-01 22:41:50 +0600

    server side

--- Dev-Corner-BK-Server/index.js
+++ Dev-Corner-BK-Server/index.js
@@ -4,7 +4,7 @@ import 'dotenv/config'; //to use environment variables
 
 
 const server = express();
-let PORT = process.env.PORT || 4000;
+let PORT = process.env.PORT || 3000;
 
 
 server.use(express.json()) //middleware - for parsing application/json
@@ -20,23 +20,31 @@ mongoose.connect(process.env.DB_LOCATION, {
 
 
 server.get('/', (req, res) => {
+
   res.send('Server is running');
+
+});
+server.get('/signUp', (req, res) => {
+
+  res.send('sign up Server is running');
+
 });
 
 
 server.post("/signUp", (req,res)=>{
 
-  res.json(req.body)
-  // let {fullname, email,password} = req.body;
-  // fullname = fullname.trim(); if(!fullname || fullname.length < 3){
-  //   return  res.status(403).json({"error":"Fullname must be at least 3 letters long"})
-  // }
+  let {fullname, email, password} = req.body;
 
-  // return res.status(200).json({"status":"okay"})
+
+ if( fullname.length < 3){
+    return  res.status(403).json({"error":"Fullname must be at least 3 letters long"})
+  }
+ if(!email.length < 3){
+    return  res.status(403).json({"error":"Enter Email."})
+  }
+
+  return res.status(200).json({"status":"okay"})
 })
-
-
-
 
 server.listen(PORT, () => {
   console.log(`listening port to ${PORT}`);
