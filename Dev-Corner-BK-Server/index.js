b39d47501421bd727209d8431d2375f0302b147b
khadiza21 <bibikhadiza474@gmail.com>
2024-10-01 20:11:20 +0600

    server

--- Dev-Corner-BK-Server/index.js
+++ Dev-Corner-BK-Server/index.js
@@ -1,9 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
+import 'dotenv/config';
+
 
 const server = express();
 
-let PORT = 4000;
+mongoose.connect(process.env.DB_LOCATION, {
+  autoIndex: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+
+
+server.post("/signUp", (req,res)=>{
+  res.json(req.body)
+})
+
+
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log("listening port " + PORT);
+  console.log(`listening port to ${PORT}`);
 });
